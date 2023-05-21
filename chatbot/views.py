49398a17aec8731970aa9766755e26d1d936a33b
behradpeikani <behradpeikani@gmail.com>
2023-05-21 21:27:04 +0430

    add form

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-	return render(request, 'chatbot/home.html')
+	if request.method == 'POST':
+		question = request.POST.get('question')
+		return render(request, 'chatbot/home.html', {"question": question})
+
+	return render(request, 'chatbot/home.html', {"question": question})
