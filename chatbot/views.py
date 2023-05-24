9f85cf20da923b7d0f2fa25591887f4135647eff
behradpeikani <behradpeikani@gmail.com>
2023-05-24 17:24:03 +0430

    error handling in views.py

--- chatbot/views.py
+++ chatbot/views.py
@@ -17,31 +17,35 @@ def home(request):
 		# Openai API 
 		openai.api_key = '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'
 		openai.Model.list()
-		response = openai.Completion.create(
-			model='text-davinci-003',
-			prompt=question,
-			temperature=0,
-			max_tokens=60,
-			top_p=1.0,
-			frequency_penalty=0.0,
-			presence_penalty=0.0
-			)
+		try:
+			response = openai.Completion.create(
+				model='text-davinci-003',
+				prompt=question,
+				temperature=0,
+				max_tokens=60,
+				top_p=1.0,
+				frequency_penalty=0.0,
+				presence_penalty=0.0
+				)
 
-		# parse the response
-		response = (response["choices"][0]["text"]).strip()
+			# parse the response
+			response = (response["choices"][0]["text"]).strip()
 
-		# logic for past_responses
-		if "skmvosno" in past_responses:
-			past_responses = response
-		else:
-			past_responses = f"{past_responses}</br></br>{response}"
+			# logic for past_responses
+			if "skmvosno" in past_responses:
+				past_responses = response
+			else:
+				past_responses = f"{past_responses}</br></br>{response}"
 
-		# save to database
-		record = History(question=question, answer=response)
-		record.save()
+			# save to database
+			record = History(question=question, answer=response)
+			record.save()
 
-		return render(request, 'chatbot/home.html', {"question": question, 
-		"response": response, "past_responses": past_responses})
+			return render(request, 'chatbot/home.html', {"question": question, 
+			"response": response, "past_responses": past_responses})
+		except Exception as e:
+			return render(request, 'chatbot/home.html', {"question": e, 
+			"response": response, "past_responses": past_responses})
 
 	return render(request, 'chatbot/home.html', {"question": question, 
 		"response": response, "past_responses": past_responses})
